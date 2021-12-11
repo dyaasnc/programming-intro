81e24ba9582a5b64e0500058b311f380c11fa4b0
unknown <dyaasenouci@gmail.com>
2021-12-11 15:27:32 +0100

    learn js

---
+++ script/app.js
@@ -0,0 +1,13 @@
+function printIamDev() {
+    var x = 0
+    var y = 0
+    while (x < 5 && y < 6) {
+        console.log("i am dyaa", x)
+        x++
+        y = y + 2
+    }
+    console.log("x =>", x)
+    console.log("y =>", y)
+
+}
+printIamDev()
